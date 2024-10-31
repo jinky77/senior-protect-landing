03ed2ad214528cba44ac2a3350775b5fcf2ee252
Paul Levent <paullevent2014@u.northwestern.edu>
2024-10-31 21:42:16 +0100

    Added content

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
+  output: "server",
+  adapter: cloudflare(),
 });
