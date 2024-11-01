175ae7f215430fa16f23c3d5ed11f2261d606bbf
Paul Levent <paullevent2014@u.northwestern.edu>
2024-11-01 12:20:39 +0100

    Cleaned project for Cloudfalre deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 
-import cloudflare from "@astrojs/cloudflare";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
-  output: "server",
-  adapter: cloudflare(),
 });
