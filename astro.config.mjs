b8aa3ca61c3d6248bbcf130c0353891402bd8bfa
Paul Levent <paul.levent@sciencespo.fr>
2024-11-22 10:50:54 +0100

    Fixed typo

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://senior-protect.tech/",
+  site: "https://senior-protect.tech",
   integrations: [tailwind(), mdx(), sitemap(), icon()],
 });
